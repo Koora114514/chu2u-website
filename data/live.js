4881366998e8fd713d8a1352c494bfa362b0e814
chu2u-site <chu2u@local>
2026-09-15 02:10:49 +0800

    自动更新 2026-09-15 02:10

--- data/live.js
+++ data/live.js
@@ -1,1 +1,1 @@
-window.CHU2U_LIVE={"live":false,"title":"开开心心！⭐","online":0,"area":"虚拟主播","start":-1,"checked":"2026-09-15 00:32:00"};
+window.CHU2U_LIVE={"live":false,"title":"开开心心！⭐","online":0,"area":"虚拟主播","start":-1,"checked":"2026-09-15 02:10:46"};
